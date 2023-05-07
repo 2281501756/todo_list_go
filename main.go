5f7717d8abd08dd0cfdea3f8532f01664eec8f4f
董浩 <2281501756@qq.com>
2023-05-07 16:34:28 +0800

    clear

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"todo_list/conf"
-	"todo_list/routs"
-)
-
 func main() {
-	conf.Init()
-	r := routs.CreateRoutes()
-	r.Run(conf.AppPort)
+
 }
--- model/migrate.go
+++
@@ -1,5 +0,0 @@
-package model
-
-func Migration() {
-	DB.AutoMigrate(&User{}, &Task{})
-}
